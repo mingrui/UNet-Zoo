fix bug: bdclstm generating mask of wrong dimensions

main_small.py
@@ -27,15 +27,17 @@ from tqdm import tqdm
 import scipy.io as sio
 import numpy as np
 
+import shutil
+
 from plot_ims import save_prediction, plot_test
 
 # %% import transforms
 
 # %% Training settings
 parser = argparse.ArgumentParser(description='UNet+BDCLSTM for BraTS Dataset')
-parser.add_argument('--batch-size', type=int, default=1, metavar='N',
+parser.add_argument('--batch-size', type=int, default=4, metavar='N',
                     help='input batch size for training (default: 64)')
-parser.add_argument('--test-batch-size', type=int, default=1, metavar='N',
+parser.add_argument('--test-batch-size', type=int, default=4, metavar='N',
                     help='input batch size for testing (default: 1000)')
 parser.add_argument('--train', action='store_true', default=False,
                     help='Argument to train model (default: False)')
@@ -180,12 +182,12 @@ def test(train_accuracy=False, save_output=False):
         if args.cuda:
             image, mask = image.cuda(), mask.cuda()
 
-        image, mask = Variable(image, volatile=True), Variable(
-            mask, volatile=True)
+        with torch.no_grad():
+            image, mask = Variable(image), Variable(mask)
 
         output = model(image)
 
-        test_loss += criterion(output, mask).data[0]
+        test_loss += criterion(output, mask).item()
 
         output.data.round_()
 
@@ -259,12 +261,22 @@ def save_test():
 def predict():
     loader = pred_loader
 
+    file_names = dset_pred.get_file()
+    save_dir = PRED_OUTPUT
+    base_name = 'OutMasks-unetsmall'
+    out_folder = BATCH_OUT_FOLDER
+
+    if os.path.exists(BATCH_OUT_FOLDER):
+        shutil.rmtree(BATCH_OUT_FOLDER)
+    os.mkdir(BATCH_OUT_FOLDER)
+
     for batch_idx, image in tqdm(enumerate(loader)):
 
         if args.cuda:
             image = image.cuda()
 
-        image= Variable(image, volatile=True)
+        with torch.no_grad():
+            image= Variable(image)
 
         output = model(image)
 
@@ -274,11 +286,6 @@ def predict():
                 output.data.byte().cpu().numpy())
         np.save(os.path.join(BATCH_OUT_FOLDER, '{}-unetsmall-batch-{}-images.npy'.format(args.save,batch_idx)),
                 image.data.float().cpu().numpy())
-
-    file_names = dset_pred.get_file()
-    save_dir = PRED_OUTPUT
-    base_name = 'OutMasks-unetsmall'
-    out_folder = BATCH_OUT_FOLDER
 
     save_prediction(file_names, save_dir, base_name, out_folder)
 

models.py
@@ -81,8 +81,12 @@ class UNet(nn.Module):
 class UNetSmall(nn.Module):
     def __init__(self, num_channels=1, num_classes=2):
         super(UNetSmall, self).__init__()
+        features = 16
+
+        # by default:
         # num_feat = [32, 64, 128, 256]
-        num_feat = [16, 32, 64, 128]
+        # if gpu memory not enough, set features to 16
+        num_feat = [features, 2*features, 2*2*features, 2*2*2*features]
 
         self.down1 = nn.Sequential(Conv3x3Small(num_channels, num_feat[0]))
 

plot_ims.py
@@ -240,6 +240,8 @@ def save_prediction(file_names, segmentation_prediction_dir, base_name, outmask_
                 plt2 = np.squeeze(final_masks[i][s, :, :])
 
             image_data = plt1
+            # print('plt3.shape : ', plt3.shape)
+            # print('plt3 : ', plt3)
             segment_data = np.ma.masked_where(plt3 < 0.9, plt3)
 
             if has_test_set:
@@ -248,8 +250,18 @@ def save_prediction(file_names, segmentation_prediction_dir, base_name, outmask_
             if has_test_set:
                 ax_params.append([image_data, segment_data, file_names[count], masked_data])
             else:
+                # print('len(file_names) : ', len(file_names), 'count : ', count)
                 ax_params.append([image_data, segment_data, file_names[count]])
-                np.savetxt(os.path.join(segmentation_prediction_dir, file_names[count]+'.txt'), segment_data[1].astype(int), fmt='%i',delimiter=',')
+                # print('image_data.shape : ', image_data.shape)
+                # print('segment_data.shape : ', segment_data.shape)
+                # print('segment_data.max() : ', segment_data.max())
+                # print('segment_data : ', segment_data)
+                if len(segment_data.shape) == 2:
+                    # bdclstm
+                    np.savetxt(os.path.join(segmentation_prediction_dir, os.path.splitext(file_names[count])[0]+'.txt'), segment_data.astype(int), fmt='%i',delimiter=',')
+                elif len(segment_data.shape) == 3:
+                    # unet
+                    np.savetxt(os.path.join(segmentation_prediction_dir, os.path.splitext(file_names[count])[0]+'.txt'), segment_data[1].astype(int), fmt='%i',delimiter=',')
 
             count += 1
 
@@ -267,9 +279,11 @@ def save_prediction(file_names, segmentation_prediction_dir, base_name, outmask_
             if has_test_set:
                 ax.imshow(ax_param[3], cmap=cm.jet, alpha=0.3)
             # TODO: shape problem
-            try:
+            if len(ax_param[1].shape) == 3:
+                # unet
                 ax.imshow(ax_param[1][1], cmap=cm.autumn, alpha=0.15)
-            except:
+            elif len(ax_param[1].shape) == 2:
+                # bdclstm
                 ax.imshow(ax_param[1], cmap=cm.autumn, alpha=0.15)
             plt.savefig(os.path.join(segmentation_prediction_dir, ax_param[2]))
             plt.close()